d3e8b51108a75a67b97dd92d2260bb0cdd41454c
HateM <dimavybornov@gmail.com>
2025-03-14 14:41:57 +0100

    Props
    Making Components Reusable with Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
+import atomImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
 const ReactDescription = ["Fundamental", "Core", "Crucial"];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 
+function CoreConcepts(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
 
 function Header() {
   const description = ReactDescription[genRandomInt(2)]
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={atomImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for
@@ -24,7 +35,17 @@ function App() {
     <div>
       <Header />
       <main>
+        <section id="core-concepts">
         <h2>Time to get started!</h2>
+        <ul>
+          <CoreConcepts 
+            title='Components'
+            description='The core UI building block'
+            image={componentsImg}/>
+          <CoreConcepts />
+          <CoreConcepts />
+        </ul>
+        </section>
       </main>
     </div>
   );
